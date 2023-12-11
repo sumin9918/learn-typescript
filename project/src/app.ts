893e662b78684160ad89278a5715fa299fafbb55
aesumin <104312167+aesumin@users.noreply.github.com>
2023-12-12 00:11:32 +0900

    [type] strict option

--- project/src/app.ts
+++ project/src/app.ts
@@ -8,8 +8,8 @@ import {
 } from './type/covid';
 
 // utils
-function $(selector: string) {
-  return document.querySelector(selector);
+function $<T extends Element = HTMLDivElement>(selector: string) {
+  return document.querySelector(selector) as T;
 }
 function getUnixTimestamp(date: number | string | Date) {
   return new Date(date).getTime();
@@ -18,13 +18,13 @@ function getUnixTimestamp(date: number | string | Date) {
 // DOM
 // document.querySelector에 의해 자동적으로 Element 타입으로 자동 추론
 // 따라서, 더 자세한 타입을 설정
-const confirmedTotal = $('.confirmed-total') as HTMLSpanElement;
-const deathsTotal = $('.deaths') as HTMLParagraphElement;
-const recoveredTotal = $('.recovered') as HTMLParagraphElement;
-const lastUpdatedTime = $('.last-updated-time') as HTMLParagraphElement;
-const rankList = $('.rank-list');
-const deathsList = $('.deaths-list');
-const recoveredList = $('.recovered-list');
+const confirmedTotal = $<HTMLSpanElement>('.confirmed-total');
+const deathsTotal = $<HTMLParagraphElement>('.deaths');
+const recoveredTotal = $<HTMLParagraphElement>('.recovered');
+const lastUpdatedTime = $<HTMLParagraphElement>('.last-updated-time');
+const rankList = $<HTMLOListElement>('.rank-list');
+const deathsList = $<HTMLOListElement>('.deaths-list');
+const recoveredList = $<HTMLOListElement>('.recovered-list');
 const deathSpinner = createSpinnerElement('deaths-spinner');
 const recoveredSpinner = createSpinnerElement('recovered-spinner');
 
@@ -59,7 +59,7 @@ enum CovidStatus {
 }
 
 function fetchCountryInfo(
-  countryCode: string,
+  countryCode: string | undefined,
   status: CovidStatus,
 ): Promise<AxiosResponse<CountryInfoResponse>> {
   // status params: confirmed, recovered, deaths
@@ -78,13 +78,15 @@ function initEvents() {
   rankList.addEventListener('click', handleListClick);
 }
 
-async function handleListClick(event: MouseEvent) {
+async function handleListClick(event: Event) {
   let selectedId;
   if (
     event.target instanceof HTMLParagraphElement ||
     event.target instanceof HTMLSpanElement
   ) {
-    selectedId = event.target.parentElement.id;
+    selectedId = event.target.parentElement
+      ? event.target.parentElement.id
+      : undefined;
   }
   if (event.target instanceof HTMLLIElement) {
     selectedId = event.target.id;
@@ -191,7 +193,7 @@ async function setupData() {
 }
 
 function renderChart(data: number[], labels: string[]) {
-  const ctx = ($('#lineChart') as HTMLCanvasElement).getContext('2d');
+  const ctx = $<HTMLCanvasElement>('#lineChart').getContext('2d');
   Chart.defaults.color = '#f5eaea';
   Chart.defaults.font.family = 'Exo 2';
   new Chart(ctx, {
